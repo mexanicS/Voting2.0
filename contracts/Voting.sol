7ca42e8f79387d90a032291582e1d8ca3c636287
Степан Инин <inin_01@mail.ru>
2022-04-26 00:39:33 +0700

    add function vote,withdracomission, finish voting

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,12 +3,20 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Voting {
   address public owner;
-  uint constant timeToVote = 3 days;
+  address payable winCandidate;
+  uint256 constant timeToVote = 3 days;
+  uint256 public maxVotes;
+  uint256 private comission;
+
   Votings[] public votings;
-  mapping(uint256 => mapping(uint256 => Candidate)) public candidate;
+  mapping(uint256 => Candidate[]) public candidate;
+  mapping(uint256 => mapping(address => Electorate)) public electorates;
 
   event VotingCreated(uint256 index);
   event AddCandidate(uint256 index);
+  event Voted(uint indexed id, address indexed voter);
+  event VotingsFinished(uint256 indexed id,address indexed winCandidate);
+  event Withdraw(address indexed account, uint256 amount);
 
   constructor()  {
     owner = msg.sender;
@@ -32,7 +40,6 @@ contract Voting {
     uint256 totalVotingVotes;
     uint256 totalCandidate;
     uint256 deposit;
-    //uint256 comission;
   }
 
   struct Electorate {
@@ -69,7 +76,7 @@ contract Voting {
   }
 
   function addCandidate (uint256 _votingId, string calldata _name, address payable _candidateAddress) public {
-    require(votings[_votingId].status==VotingStatus.NOT_ACTIVE,"Voting is not ACTIVE");
+    require(votings[_votingId].status==VotingStatus.NOT_ACTIVE,"Voting is ACTIVE");
     //require(votings[_votingId].endTimeOfVoting >= block.timestamp,"Start voting first.");
     for (uint256 i = 0; i < votings[_votingId].totalCandidate; i++) {
       if(_candidateAddress == candidate[_votingId][i].candidateAddress){
@@ -79,9 +86,54 @@ contract Voting {
     candidate[_votingId][votings[_votingId].totalCandidate].name = _name;
     candidate[_votingId][votings[_votingId].totalCandidate].candidateAddress = _candidateAddress;
 
-
     votings[_votingId].totalCandidate++;
-    //emit AddCandidate(candidate[_votingId].length);
+  }
+
+  function Vote(uint256 _votingId, uint256 _numberCandidate) payable public {
+    require(votings[_votingId].totalCandidate >= _votingId, "Voting does not exist");
+    //Election storage election = _election[electionId];
+    require(votings[_votingId].endTimeOfVoting >= block.timestamp,"The voting is over");
+    require(!electorates[_votingId][msg.sender].isVoted,"Have you already voted");
+    require(msg.value >= .01 ether,"Insufficient funds for voting" );
+
+    votings[_votingId].totalVotingVotes++;
+    votings[_votingId].deposit += msg.value;
+    
+    electorates[_votingId][msg.sender].electorateAddress = msg.sender;
+    electorates[_votingId][msg.sender].isVoted = true;
+    
+    candidate[_votingId][_numberCandidate].totalCandidateVotes++;
+
+    emit Voted(_votingId, msg.sender);
+  }
+
+  function finishVoting(uint256 _votingId) public {
+    require(votings[_votingId].status == VotingStatus.ACTIVE,"Voting is still underway.");
+    require(votings[_votingId].endTimeOfVoting <= block.timestamp,"Voting is active.");
+
+    for (uint256 i = 0; i <votings[_votingId].totalCandidate; i++) {
+      if(candidate[_votingId][i].totalCandidateVotes > maxVotes){
+        maxVotes = candidate[_votingId][i].totalCandidateVotes;
+        winCandidate = candidate[_votingId][i].candidateAddress;
+      }
+    }
+
+    comission = votings[_votingId].deposit /10;
+    votings[_votingId].deposit -= comission;
+
+    winCandidate.transfer(votings[_votingId].deposit);
+
+    votings[_votingId].deposit = 0;
+    votings[_votingId].status = VotingStatus.COMPLETED;
+    emit VotingsFinished(_votingId,winCandidate);
+  }
+
+  function withdrawComission(uint256 _votingId, address payable _to) public requireOwner {
+    require(votings[_votingId].status==VotingStatus.COMPLETED,"Voting is still ACTIVE");
+    _to.transfer(comission);
+    emit Withdraw(_to, comission);
+    comission = 0;
+    
   }
 
   function infVoting(uint256 _votingId) public view returns (Votings memory _votings) {
@@ -91,4 +143,8 @@ contract Voting {
   function infCandidate (uint256 _votingId, uint256 _numberCandidate) external view returns (Candidate memory _candidate){
     return candidate[_votingId][_numberCandidate];
   }
+
+  
+
+
 }
